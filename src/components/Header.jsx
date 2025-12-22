8be584533c670192005a52548a0c8e019be88a04
Saleh545 <141879902+Saleh545@users.noreply.github.com>
2025-12-23 01:36:57 +0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import '../main.scss'
+import '../styles/component/header.scss'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <div className="container">
+          <div className="header-flex">
+            <div className="header-left">
+              
+              <img src="src/assets/uros.jpg" alt="" />
+            </div>
+            <div className="header-right">
+              <ul>
+                <li>
+                  <Link>Haqqımızda</Link>
+                </li>
+                <li>
+                  <Link>Funksiyalar</Link>
+                </li>
+                <li>
+                  <Link>İntegrasiyalar</Link>
+                </li>
+                <li>
+                  <Link>Partnyorlar</Link>
+                </li>
+                <li>
+                  <Link>Niyə UR-OS</Link>
+                </li>
+                <li>
+                  <Link>Təhlükəsizlik</Link>
+                </li>
+              </ul>
+
+              <button className='lang' >AZ</button>
+
+              <div className="login">
+                <Link to="/login">Login</Link>
+                <Link to="/sign-up">Sign up</Link>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
