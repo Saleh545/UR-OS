411e7f7d41ca830edcc85045af9860b4be62eede
Saleh545 <141879902+Saleh545@users.noreply.github.com>
2026-01-05 04:20:51 +0400

    faf

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,64 +1,63 @@
 import React from 'react'
 import '../styles/pages/signIn.scss'
 import signImg from '../assets/auth-v2-login-illustration-dark.png';
+import Header from '../components/Header';
 
 const SignIn = () => {
-  return (
-    <>
-    <div className="signin">
-      <div className="signin__container">
+    return (
+        <>
+            {/* <Header/> */}
+            <div className="signin">
+                <div className="signin__container">
 
-        {/* SOL HİSSƏ */}
-        <div className="signin__preview">
+                    <div className="signin__preview">
 
-          
 
-            {/* Illustration */}
-            <img
-              src={signImg}
-              alt="Login Illustration"
-              className="signin__image"
-            />
 
-          </div>
+                        <img
+                            src={signImg}
+                            alt="Login Illustration"
+                            className="signin__image"
+                        />
 
-        {/* SAĞ HİSSƏ */}
-        <div className="signin__form">
-          <h1>Welcome to UR-OS 👋</h1>
-          <p className="subtitle">Please sign in to your account</p>
+                    </div>
 
-         
+                    <div className="signin__form">
+                        <h1>Welcome to UR-OS 👋</h1>
+                        <p className="subtitle">Please sign in to your account</p>
 
-          <form>
-            <div className="field">
-              <label>Email</label>
-              <input type="email" placeholder="admin@demo.com" />
+
+
+                        <form>
+                            <div className="field">
+                                <label>Email</label>
+                                <input type="email" placeholder="admin@demo.com" />
+                            </div>
+
+                            <div className="field">
+                                <label>Password</label>
+                                <input type="password" placeholder="••••••••" />
+                            </div>
+
+                            <div className="options">
+                                <label>
+                                    <input type="checkbox" /> Remember me
+                                </label>
+                                <a href="#">Forgot password?</a>
+                            </div>
+
+                            <button type="submit">Login</button>
+                        </form>
+
+                        <p className="register">
+                            New here? <a href="#">Create an account</a>
+                        </p>
+                    </div>
+
+                </div>
             </div>
-
-            <div className="field">
-              <label>Password</label>
-              <input type="password" placeholder="••••••••" />
-            </div>
-
-            <div className="options">
-              <label>
-                <input type="checkbox" /> Remember me
-              </label>
-              <a href="#">Forgot password?</a>
-            </div>
-
-            <button type="submit">Login</button>
-          </form>
-
-          <p className="register">
-            New here? <a href="#">Create an account</a>
-          </p>
-        </div>
-
-      </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default SignIn
